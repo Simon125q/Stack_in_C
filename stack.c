b1b61419f53188d83d346fed0b6facadb76836eb
Simon125q <106029626+Simon125q@users.noreply.github.com>
2023-10-12 08:17:33 +0200

    cleaned code

--- stack.c
+++ stack.c
@@ -6,18 +6,20 @@
 #define INITIAL_SIZE 2
 #define STACK_GROWTH_RATE 2
 
-void init(Stack* s) {
+void init(Stack *s)
+{
     s->top_index = 0;
     s->size = INITIAL_SIZE;
-    s->elements = (int *) malloc(s->size * sizeof(int));
+    s->elements = (int *)malloc(s->size * sizeof(int));
     checkAllocation(s);
     printf("Stack created, memory allocated\n");
 }
 
-void destroy(Stack* s) {
-    if (s->top_index < 0) {
+void destroy(Stack *s)
+{
+    if (s->top_index < 0)
+    {
         printf("Stack is already destroyed\n");
-        //free(s->elements);
         abort();
     }
     s->top_index = -1;
@@ -25,45 +27,55 @@ void destroy(Stack* s) {
     printf("Stack destroyed\n");
 }
 
-void push(Stack* s, int element) {
-    if (s->size <= s->top_index){
+void push(Stack *s, int element)
+{
+    if (s->size <= s->top_index)
+    {
         s->size *= STACK_GROWTH_RATE;
-        s->elements = (int *) realloc(s->elements, s->size * sizeof(int));
+        s->elements = (int *)realloc(s->elements, s->size * sizeof(int));
         printf("Memory reallocated\n");
         checkAllocation(s);
     }
-    
+
     s->elements[s->top_index++] = element;
     printf("Element %d pushed to stack\n", element);
 }
 
-int pop(Stack* s) {
-    if (!isEmpty(s)){
+int pop(Stack *s)
+{
+    if (!isEmpty(s))
+    {
         s->top_index--;
         int element = s->elements[s->top_index];
         return element;
     }
-    else {
+    else
+    {
         printf("Stack is empty\n");
         destroy(s);
-        //free(s->elements);
         abort();
     }
 }
 
-bool isEmpty(Stack* s) {
-    if (s->top_index < 0) {
+bool isEmpty(Stack *s)
+{
+    if (s->top_index < 0)
+    {
         printf("Stack doesnt exist");
         abort();
     }
-    else if (s->top_index == 0) {
+    else if (s->top_index == 0)
+    {
         return true;
     }
-    else return false;
+    else
+        return false;
 }
 
-void checkAllocation(Stack* s) {
-    if (s->elements == NULL) {
+void checkAllocation(Stack *s)
+{
+    if (s->elements == NULL)
+    {
         printf("Allocation error\n");
         abort();
     }
--- stack.h
+++ stack.h
@@ -1,14 +1,15 @@
 #pragma once
 
-typedef struct Stack {
+typedef struct Stack
+{
     int top_index;
     int size;
-    int * elements;
+    int *elements;
 } Stack;
 
-void init(Stack* s);
-void destroy(Stack* s);
-void push(Stack* s, int element);
-int pop(Stack* s);
-bool isEmpty(Stack* s);
-void checkAllocation(Stack* s);
+void init(Stack *s);
+void destroy(Stack *s);
+void push(Stack *s, int element);
+int pop(Stack *s);
+bool isEmpty(Stack *s);
+void checkAllocation(Stack *s);
